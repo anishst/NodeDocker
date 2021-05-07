21ad603031f4469026ca1e92dcaa5073c3de1faa
sebastian-anish <sebastian_anish@bah.com>
2021-05-07 11:47:17 -0400

    moved env vars to config file

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose');
+// import env vars from config file
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT} = require("./config/config");
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 // 172.26.0.2 = docker mongo container ip address
-// mongo refers to ip address of mongo container
+// mongo refers to ip address of mongo container 
 mongoose
-    .connect("mongodb://anish:mypassword@mongo:27017/?authSource=admin")
+    .connect(mongoURL, {
+        // get rid of warnings
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
     .then(() => console.log("Successfully connected to Mongo DB"))
     .catch((e) => console.log(e));
 
